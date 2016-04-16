7330ca0d3433bd4a1d0a166763c925c68bc3995d
José Rafael de Farias Brito <1521360022@unipe.local>
2016-04-16 16:07:01 -0300

    Cria classe para exibir o resultado do IMC na branch bFeature

---
+++ src/main/java/br/gov/unipe/App.java
@@ -0,0 +1,13 @@
+package br.gov.unipe;
+
+/**
+ * Classe que exibe o IMC da aula de Gerência de Configuração na Unipê
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {		
+        System.out.println( "O seu IMC deu: " + calculaIMC(75,1.70) );
+    }
+}
